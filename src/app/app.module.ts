f98555d9bad50e6c5e99cd782890e3c6c5c23601
Juan Manuel comellas <jm_comellas@hotmail.com>
2021-02-26 21:09:38 -0300

    Se implemento la seleccion de temas y la vinculacion entre componentes de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { IncrementadorComponent } from './components/incrementador/incrementador.component';
 
 //temporal
 import { FormsModule } from '@angular/forms';
+
+//services
+import { ServiceModule } from './services/service.module';
 
 
 @NgModule({
@@ -27,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/settings/settings.service.ts
@@ -0,0 +1,51 @@
+import { temporaryAllocator } from '@angular/compiler/src/render3/view/util';
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  
+  ajustes:Ajustes = {
+    temaUrl:"assets/css/colors/default.css",
+    tema: "default"
+  }
+
+  constructor(@Inject(DOCUMENT)private _document:Document) { 
+    this.cargarAjustes();
+  }
+
+  guardarAjustes(){
+    localStorage.setItem('ajustes',JSON.stringify(this.ajustes))
+  }
+
+  cargarAjustes()
+  {
+    if(localStorage.getItem('ajustes'))
+    {
+      this.ajustes = JSON.parse(localStorage.getItem('ajustes')!);
+      this.aplicarTema(this.ajustes.tema);
+    }
+  }
+
+  aplicarTema(tema:string)
+  {
+    let url = 'assets/css/colors/'+tema+'.css';
+    this._document.getElementById('tema')?.setAttribute('href',url);
+    
+    this.ajustes.tema=tema;
+    this.ajustes.temaUrl=url;
+
+    this.guardarAjustes();
+  }
+
+}
+
+
+
+
+interface Ajustes{
+  temaUrl:string;
+  tema:string;
+}
